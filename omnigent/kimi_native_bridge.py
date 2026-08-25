7b945216841a01455bf96a9bc0bc700c0da3a37d
Nick Isaacs <nick.isaacs@datadoghq.com>
2026-08-25 07:17:11 -0600

    refactor: type Kimi bridge payloads (#3825)
    Signed-off-by: Pat Sukprasert <pat.sukprasert@databricks.com>
    
    Co-authored-by: Pat Sukprasert <pattara.sk127@gmail.com>

--- omnigent/kimi_native_bridge.py
+++ omnigent/kimi_native_bridge.py
@@ -19,9 +19,12 @@ import subprocess
 import tempfile
 import time
 from pathlib import Path
+from typing import TypeAlias
 
 from omnigent._platform import stable_user_id
 from omnigent.json_types import JsonObject as _JsonObject
+
+_JsonObject: TypeAlias = dict[str, object]
 
 #: Env var carrying the bridge dir into the harness executor process.
 BRIDGE_DIR_ENV_VAR = "HARNESS_KIMI_NATIVE_BRIDGE_DIR"
